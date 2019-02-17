8239d9b426b905ffb08fb23d70a159522903695b
Takxazis <Takxazis@gmail.com>
2019-02-17 19:14:38 +0200

    better not better

--- script.js
+++ script.js
@@ -1,56 +1,64 @@
 
-// короч. по дому все норм, находит, вставляет, не могу чек сделать на длину, хоть убей. 
-// пробовал всяко, менял местами всё че можно, был на стак оверфлоу, там оказалось также я как замутил... у них работает.
-// пробовал через this пофиг как его запрашивал через родителей и классы, айди. добавлял елсы, перехадживал по разному из дефолта в хайд.
-// и в див я его оборачивал... 
-// внизу пример this
-// горит пукан и у тебя глаза горят
-// а хочу я, что-бы у меня текстик появлялся рядом с инпутом если длина не подходит
 
 
 $(function () {
     
-    // $('#errorUsername').html('test'); // находит, вставляет куда надо.
+    $('#errorUsername').html('must be 4-18 char').hide();
+    $('#errorPassword').html('must be min 8 char').hide();
+    $('#errorRetypePassword').html('password does not mutch').hide();
+    $('#errorEmail').html('invalid Email').hide();
     
 
     $('#username').focusout(function () {
-        console.log('focusout works'); // вона працюе
-        
-        
-            var usernameLenght = $('#username').val().lenght;
-            
-         
-            if(usernameLenght < 4 || usernameLenght > 5){   // ВОТ ТУТ ДОЛЖНО РАБОТАТЬ!!!!!!! 
-                $('#errorUsername').html('test');
-                      
-            }
-            
-        
-        
- 
+        console.log('USERNAME focusout works'); 
+        checkUsername();           
  });
 
-//  $('#username').focusout(function () {
-//     console.log('focusout works');
-    
-    
-//         var usernameLenght = $('#username').val().lenght;
-        
-     
-//         if($(this).val().lenght < 4 || $(this).val().lenght > 5){
-//             $('#errorUsername').html('test');
-                  
-        
-//         }
+ $('#password').focusout(function () {
+    console.log('PASSWORD focusout works'); 
+    checkPassword();           
+});
 
+$('#retypePassword').focusout(function () {
+    console.log('RETYPE PASSWORD focusout works'); 
+    checkRetypePassword();           
+});
 
+$('#email').focusout(function () {
+    console.log('EMAIL focusout works'); 
+    // checkEmail();           
+});
 
+function checkUsername(){
+    let usernameLength = $('#username').val().length;
+    if (usernameLength < 4 || usernameLength > 18){
+        $('#errorUsername').show();
+    }else{
+        $('#errorUsername').hide();
+    }
+}
 
+function checkPassword(){
+    let passwordLength = $('#password').val().length;
+    if (passwordLength < 8){
+        $('#errorPassword').show();
+    }else{
+        $('#errorPassword').hide();
+    }
+}
 
+function checkRetypePassword(){  
+    let pass = $('#password').val();
+    let rePass = $('#retypePassword').val();
+    if ( pass != rePass){
+        $('#errorRetypePassword').show();
+    }else{
+        $('#errorRetypePassword').hide();
+    }
+}
 
+function checkEmail(){
 
-
-
-
+}
 
 })
