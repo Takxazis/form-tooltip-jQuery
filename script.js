1b2dfb6a224756a5df79b45a17445bd3cac0a70d
Takxazis <Takxazis@gmail.com>
2019-02-20 13:55:52 +0200

    alomoust done. no css.

--- script.js
+++ script.js
@@ -3,16 +3,21 @@
 
 $(function () {
 
+    //form section
+
     $('#errorUsername').html('must be 4-18 char').hide();
     $('#errorPassword').html('must be min 8 char').hide();
     $('#errorRetypePassword').html('password does not mutch').hide();
     $('#errorEmail').html('invalid Email').hide();
 
-$('#divka').click(function(){
-    $('#password').prop('type', 'text');
-    $('#retypePassword').prop('type', 'text');
-    
-})
+    $('#divka').click(function () {
+        $('#password').prop('type', 'text');
+        $('#retypePassword').prop('type', 'text');
+        console.log("password revealed");
+
+    })
+
+    //form logic
 
 
     $('#username').focusout(function () {
@@ -35,6 +40,7 @@ $('#divka').click(function(){
         checkEmail();
     });
 
+    // form functions
     function checkUsername() {
         const usernameLength = $('#username').val().length;
         if (usernameLength < 4 || usernameLength > 18) {
@@ -67,12 +73,71 @@ $('#divka').click(function(){
 
     function checkEmail() {
 
-        var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
+        const pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i);
         if (pattern.test($('#email').val())) {
             $('#errorEmail').hide();
         } else {
             $('#errorEmail').show();
         }
     }
+
+
+    // tip section
+
+    $('#username').hover(function () {
+        $('.tipUsername').css('display', 'block');
+
+    }, function () {
+        $('.tipUsername').css('display', 'none');
+    })
+
+    $(document).mousemove(function (event) {
+        var postionX = event.pageX + 5;
+        var postionY = event.pageY + 5;
+
+        $('.tipUsername').css('left', postionX + 'px').css('top', postionY + 'px');
+    })
+    $('#password').hover(function () {
+        $('.tipPassword').css('display', 'block');
+
+    }, function () {
+        $('.tipPassword').css('display', 'none');
+    })
+
+    $(document).mousemove(function (event) {
+        var postionX = event.pageX + 5;
+        var postionY = event.pageY + 5;
+
+        $('.tipPassword').css('left', postionX + 'px').css('top', postionY + 'px');
+    })
+
+
+    $('#retypePassword').hover(function () {
+        $('.tipRetypePassword').css('display', 'block');
+
+    }, function () {
+        $('.tipRetypePassword').css('display', 'none');
+    })
+
+    $(document).mousemove(function (event) {
+        var postionX = event.pageX + 5;
+        var postionY = event.pageY + 5;
+
+        $('.tipRetypePassword').css('left', postionX + 'px').css('top', postionY + 'px');
+    })
+
+    $('#email').hover(function () {
+        $('.tipEmail').css('display', 'block');
+
+    }, function () {
+        $('.tipEmail').css('display', 'none');
+    })
+
+    $(document).mousemove(function (event) {
+        var postionX = event.pageX + 5;
+        var postionY = event.pageY + 5;
+
+        $('.tipEmail').css('left', postionX + 'px').css('top', postionY + 'px');
+    })
 
 })
